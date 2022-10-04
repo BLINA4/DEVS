213e9bbffbc627f4f0a252f42a6a6c7ecd4e8967
BLIN4 <shayda.andrey@yandex.ru>
2022-10-05 01:54:21 +0300

    Static and dynamic ways implementation

---
+++ dev.h
@@ -0,0 +1,40 @@
+#include <linux/module.h>
+#include <linux/fs.h>
+#include <linux/init.h>
+#include <asm/uaccess.h>
+
+static char *hello_str = "Hello, world!\n";
+
+static ssize_t dev_read( struct file *file, char *buf, size_t count, loff_t *ppos )
+{
+  int len = strlen(hello_str);
+  
+  printk(KERN_INFO "=== read : %d\n", count);
+  
+  if (count < len)
+    return -EINVAL;
+  
+  if (*ppos != 0)
+  {
+    printk(KERN_INFO "=== read return : 0\n");
+    return 0;
+  }
+  
+  if (copy_to_user(buf, hello_str, len))
+    return -EINVAL;
+  
+  *ppos = len;
+  printk(KERN_INFO "=== read return : %d\n", len);
+  
+  return len;
+}
+
+static int __init dev_init( void );
+module_init(dev_init);
+
+static void __exit dev_exit( void );
+module_exit(dev_exit);
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("BLIN4IK97");
+
